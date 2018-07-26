11cf0a1c3b6d34cfb6cc552ef3ce657bde34213b
sev-07 <36957022+sev-07@users.noreply.github.com>
2018-07-26 09:16:42 +0300

    add readfile and print contents functionality

--- hello.js
+++ hello.js
@@ -1,8 +1,16 @@
 var fs = require('fs');
 fs.writeFile("emo.txt", "Hello World", function(err) {
-    if(err) {
-        return console.log(err);
-    }
+  if(err) {
+      return console.log(err);
+  }
 
-    console.log("The file was saved!");
+  console.log("The file was saved!");
+});
+
+fs.readFile("emo.txt", function(err1, contents) {
+  if(err1) {
+      return console.log(err1);
+  }
+
+  console.log("The saved file contains the following string: " + "\"" + contents + "\".");
 });
